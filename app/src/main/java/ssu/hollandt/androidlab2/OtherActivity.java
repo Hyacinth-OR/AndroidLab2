b3901b50332a1ebda87b455a2cae27f0e6fda9ae
analogHyperdrive <caribblacktip@yahoo.com>
2018-08-31 02:29:21 +0100

    working code complete

--- app/src/main/java/ssu/hollandt/androidlab2/OtherActivity.java
+++ app/src/main/java/ssu/hollandt/androidlab2/OtherActivity.java
@@ -1,13 +1,39 @@
 package ssu.hollandt.androidlab2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class OtherActivity extends AppCompatActivity {
+
+    public static final String NAME_KEY = "USER_NAME";
+    public TextView welcomeText;
+    private Button returnButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+        welcomeText = findViewById(R.id.welcome_text);
+        returnButton = findViewById(R.id.return_button);
+
+        final Intent newIntent = getIntent();
+        String name = newIntent.getStringExtra(NAME_KEY);
+        if(name == null || name.isEmpty()) {
+            name = "Not Bob";
+        }
+        welcomeText.setText("Welcome, " + name);
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OtherActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
