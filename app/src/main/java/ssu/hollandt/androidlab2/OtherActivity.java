0f1099c15031c19aed9da77897ac561e203d8f8c
Elliot <elliotn46@gmail.com>
2018-08-30 18:00:09 -0700

    finsihed part 2

--- app/src/main/java/ssu/hollandt/androidlab2/OtherActivity.java
+++ app/src/main/java/ssu/hollandt/androidlab2/OtherActivity.java
@@ -1,13 +1,31 @@
 package ssu.hollandt.androidlab2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.BreakIterator;
 
 public class OtherActivity extends AppCompatActivity {
 
+    private TextView welcomeText;
+
+    public static final String NAME_KEY = "USER_NAME";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+        welcomeText =  findViewById(R.id.welcome_text);
+        Intent newIntent = getIntent();
+        String name = newIntent.getStringExtra(NAME_KEY);
+
+        if(name == null || name.isEmpty())
+        {
+            name = "Not Bob";
+        }
+
+
+        welcomeText.setText ("Welcome, " + name);
     }
 }
